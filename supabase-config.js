dd959f1ab8ca62cea4b3b211bb1b91e59c9b6c24
angel20-a11 <76209750@continental.edu.pe>
2026-09-08 12:43:17 -0500

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,1 +1,4 @@
-window.ECO_ROCA_SUPABASE={url:'',publishableKey:''};
+window.ECO_ROCA_SUPABASE = {
+  url: 'https://mmkasmuvppipkqbmwrfu.supabase.co',
+  publishableKey: 'sb_publishable_y2Pzis_sd5jEKQJOa8uU1A_dbiWV33N'
+};
